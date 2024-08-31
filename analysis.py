404dca1ecc6669cf9177bfba5e169207ec04750f
Jason Galante <jasonmgalante@gmail.com>
2024-08-31 13:57:09 +0000

    added nost common values

--- analysis.py
+++ analysis.py
@@ -46,7 +46,6 @@ print("The number of HCPCS codes is:\n"
 # find and print all codexes that start with "DRG"
 # print the list
 # count the number of DRG columns
-
 drg_codes = [i for i in data if 
              i.startswith('DRG')]
 print("DRG Codes:\n" ,data[drg_codes])
@@ -59,8 +58,30 @@ missing_hcpcs = data[hcpcs_codes].isnull().sum()
 missing_drg = data[drg_codes].isnull().sum()
 
 print(f"Missing ICD Codes:\n {missing_icd}")
-print(f"Missing ICD Codes:\n {missing_clm}")
-print(f"Missing ICD Codes:\n {missing_hcpcs}")
-print(f"Missing ICD Codes:\n {missing_drg}")
+print(f"Missing CLM Codes:\n {missing_clm}")
+print(f"Missing HCPCS Codes:\n {missing_hcpcs}")
+print(f"Missing DRG Codes:\n {missing_drg}")
+
+frequency = data.columns.value_counts()
+print("MostFrequent codes:\n", frequency)
+
+icd_frequency = data[icd_codes].value_counts()
+print("ICD Codes Frequency:\n", icd_frequency)
+
+print("Top 5 Most Common ICD Codes:\n", icd_frequency.head())
+
+most_common_icd_cols = data[icd_codes].columns.value_counts().nlargest(10)
+print(most_common_icd_cols)
+
+most_common_drg_cols = data[drg_codes].columns.value_counts().nlargest()
+print(most_common_drg_cols)
+
+most_common_hcpcs_cols = data[hcpcs_codes].columns.value_counts().nlargest()
+print(most_common_hcpcs_cols)
+
+most_common_clm_cols = data[clm_codes].columns.value_counts().nlargest()
+print(most_common_clm_cols)
+
+
 
 
